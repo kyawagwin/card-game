7adb0aa03d2f11c1d01c22d22cb2354d523ea3d6
Kyaw Aung Win <kyawagwin@gmail.com>
2016-12-24 08:12:10 +0800

    watch & run : server

--- src/server/server.js
+++ src/server/server.js
@@ -4,3 +4,18 @@ console.log(`hello ${bleh}`);
 const obj = {hey: 1};
 const obj2 = {...obj, bleh: 2};
 console.log(obj2);
+
+class AppComponent {
+	static PropTypes = {
+		bleh: "whoa"
+	};
+}
+
+switch(bleh) {
+	case 1:
+		console.log("hey");
+		break;
+	case 2:
+		console.log("whoa");
+		break;
+}
