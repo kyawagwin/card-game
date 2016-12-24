6f352d62f2a8bf8667e1f596f22d58196e5a3814
Kyaw Aung Win <kyawagwin@gmail.com>
2016-12-24 08:56:34 +0800

    watch & run : tests

--- tests.js
+++ tests.js
@@ -1,1 +1,10 @@
-console.log("tests");
+require("source-map-support").install();
+const jasmine = require("jasmine");
+
+const tests = new jasmine();
+tests.loadConfig({
+	spec_dir: "./build",
+	spec_files: ["**/__tests/*.js"]
+});
+
+tests.execute();
